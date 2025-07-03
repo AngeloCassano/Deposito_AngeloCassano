683d5b8546ac252e9b0ef2cd985dd8d9a86364c6
AngeloCassano <angel96ka@gmail.com>
2025-07-03 17:53:39 +0200

    Create ProvaAstrazione.java

---
+++ 03-07 gio/ProvaAstrazione.java
@@ -0,0 +1,35 @@
+public class ProvaAstrazione {
+    public static void main(String[] args) {
+        //Animal myAnimal = new Animal();
+        Animal myPig  = new Pig();
+        Animal myDog = new Dog();
+        //myAnimal.animalSound();
+        myPig.animalSound();
+        myDog.animalSound();
+    }
+    
+
+
+}
+
+abstract class Animal{
+    public abstract void animalSound();
+    public void sleep(){
+        System.out.println("Zzz");
+    }
+
+}
+
+
+class Pig extends Animal{
+    public void animalSound(){
+        System.out.println("The pig says: wee wee");
+    }
+}
+
+class Dog extends Animal{
+    public void animalSound(){
+        System.out.println("The dog says: bow bow");
+    }
+}
+
